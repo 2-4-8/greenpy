a7e206fc73a8a447e695052f809eef0ef9228679
Alex K <ankayda@gmail.com>
2019-05-16 21:00:46 +0500

    Add format to time fields in api

--- worktime/serializers.py
+++ worktime/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from worktime.models import WorkMonth, WorkDay
 
+TIME_FORMAT = '%H:%M'
+
 
 class WorkDaySerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
+    come_time = serializers.TimeField(format=TIME_FORMAT, input_formats=TIME_FORMAT)
+    leave_time = serializers.TimeField(format=TIME_FORMAT, input_formats=TIME_FORMAT)
     work_time = serializers.IntegerField(source='get_work_time', read_only=True)
 
     class Meta:
@@ -19,8 +23,10 @@ class WorkMonthCreationSerializer(serializers.HyperlinkedModelSerializer):
     days = WorkDaySerializer(many=True, read_only=True)
     days_count = serializers.IntegerField(source='get_days_count', read_only=True)
     work_days_count = serializers.IntegerField(source='get_work_days_count', read_only=True)
-    average_come_time = serializers.TimeField(source='get_average_come_time', read_only=True)
-    average_leave_time = serializers.TimeField(source='get_average_leave_time', read_only=True)
+    average_come_time = serializers.TimeField(source='get_average_come_time', read_only=True, format=TIME_FORMAT,
+                                              input_formats=TIME_FORMAT)
+    average_leave_time = serializers.TimeField(source='get_average_leave_time', read_only=True, format=TIME_FORMAT,
+                                               input_formats=TIME_FORMAT)
     average_issues_completed = serializers.IntegerField(source='get_average_issues_completed', read_only=True)
     average_additional_issues = serializers.IntegerField(source='get_average_additional_issues', read_only=True)
     average_salary = serializers.IntegerField(source='get_average_salary', read_only=True)
