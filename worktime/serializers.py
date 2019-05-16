ba96e0090c55c51e84e05a3d21dcc92866c768b1
Alex K <ankayda@gmail.com>
2019-05-16 22:06:03 +0500

    Fix serializers

--- worktime/serializers.py
+++ worktime/serializers.py
@@ -7,8 +7,8 @@ TIME_FORMAT = '%H:%M'
 
 class WorkDaySerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    come_time = serializers.TimeField(format=TIME_FORMAT, input_formats=TIME_FORMAT)
-    leave_time = serializers.TimeField(format=TIME_FORMAT, input_formats=TIME_FORMAT)
+    come_time = serializers.TimeField(format=TIME_FORMAT, input_formats=[TIME_FORMAT,])
+    leave_time = serializers.TimeField(format=TIME_FORMAT, input_formats=[TIME_FORMAT,])
     work_time = serializers.IntegerField(source='get_work_time', read_only=True)
 
     class Meta:
@@ -23,13 +23,11 @@ class WorkMonthCreationSerializer(serializers.HyperlinkedModelSerializer):
     days = WorkDaySerializer(many=True, read_only=True)
     days_count = serializers.IntegerField(source='get_days_count', read_only=True)
     work_days_count = serializers.IntegerField(source='get_work_days_count', read_only=True)
-    average_come_time = serializers.TimeField(source='get_average_come_time', read_only=True, format=TIME_FORMAT,
-                                              input_formats=TIME_FORMAT)
-    average_leave_time = serializers.TimeField(source='get_average_leave_time', read_only=True, format=TIME_FORMAT,
-                                               input_formats=TIME_FORMAT)
-    average_issues_completed = serializers.IntegerField(source='get_average_issues_completed', read_only=True)
-    average_additional_issues = serializers.IntegerField(source='get_average_additional_issues', read_only=True)
-    average_salary = serializers.IntegerField(source='get_average_salary', read_only=True)
+    average_come_time = serializers.TimeField(source='get_average_come_time', read_only=True, format=TIME_FORMAT)
+    average_leave_time = serializers.TimeField(source='get_average_leave_time', read_only=True, format=TIME_FORMAT)
+    average_issues_completed = serializers.DecimalField(None, 2, source='get_average_issues_completed', read_only=True)
+    average_additional_issues = serializers.DecimalField(None, 2, source='get_average_additional_issues', read_only=True)
+    average_salary = serializers.DecimalField(None, 2, source='get_average_salary', read_only=True)
     average_work_time = serializers.IntegerField(source='get_average_work_time', read_only=True)
     sum_issues_completed = serializers.IntegerField(source='get_sum_issues_completed', read_only=True)
     sum_additional_issues = serializers.IntegerField(source='get_sum_additional_issues', read_only=True)
